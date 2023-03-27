6a8178713a5ee4169d10f152f63d1a8d6ff9409f
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-03-27 17:37:57 +0200

    Show done

--- src/view/ShowPage.js
+++ src/view/ShowPage.js
@@ -10,7 +10,7 @@ const showController = new ShowController();
  * @returns {ShowPage}
  */
 export default function ShowPage() {
-    
+  /*   
     // Find all shows
     showController.findAll((showResponses) => {
         console.log(showResponses);
@@ -25,22 +25,22 @@ export default function ShowPage() {
         console.log(error);
     });
 
-    // Create a show
+    // Create a show - connect to a form
     const showRequest = new ShowRequest(1, // show id
         "The Matrix",  // movieTitle
     [20, 15, 10], // reservations
     [Date.parse("2023-04-01"), Date.parse("2023-04-05"), Date.parse("2023-04-10")], // showDates
     12.50, // price
     1234 // theaterId
-    );
+    ); 
     showController.create(showRequest, (showResponse) => {
         console.log(showResponse);
     }, (error) => {
         console.log(error);
     });
 
-    // Update a show
-    showRequest.price = 16;
+     // Update a show
+    showRequest.price = 16; 
     showController.update(showRequest, (showResponse) => {
         console.log(showResponse);
     }, (error) => {
@@ -52,5 +52,5 @@ export default function ShowPage() {
         console.log("Show deleted");
     }, (error) => { 
         console.log(error);
-    });
+    });*/
 }
