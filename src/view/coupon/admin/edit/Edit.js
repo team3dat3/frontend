bf4a35b7c287c8c179f3996a32a7bd01dd97eb48
DizzyMoon <mikkel.simons1@gmail.com>
2023-03-28 20:52:12 +0200

    Added some view javascript. Not sure if I'm doing this correctly

---
+++ src/view/coupon/admin/edit/Edit.js
@@ -0,0 +1,7 @@
+import CouponController from "../../../../controller/CouponController";
+import CouponRequest from "../../../../dto/coupon/CouponRequest";
+import { loadAndRender } from '../../../../util/Render.js';
+
+export default function CouponAdminEdit(id){
+
+}
