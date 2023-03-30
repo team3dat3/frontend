e1c4acadc97a48cda02428bb5b77a7741c938e5b
Nicolai Andersen <niiicolai@gmail.com>
2023-03-30 17:05:29 +0200

    go

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
-import "./view/layout/Layout.js"; // Load the layout
 import "./config/Config.js"; // Load the config
+import "./view/layout/Layout.js"; // Load the layout
 import "./config/Router.js"; // Load the router
